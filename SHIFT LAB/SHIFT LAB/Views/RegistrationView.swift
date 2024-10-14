7a138f6ad2f27756baf16811e17a5e9e42269d3d
bdvzt <budaevazay@mail.ru>
2024-10-14 17:51:57 +0700

    registration input fields

--- SHIFT LAB/SHIFT LAB/Views/RegistrationView.swift
+++ SHIFT LAB/SHIFT LAB/Views/RegistrationView.swift
@@ -8,12 +8,48 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State private var name: String = ""
+    @State private var surname: String = ""
+    @State private var dateOfBirth: String = ""
+    @State private var password: String = ""
+    @State private var repeatPassword: String = ""
+    
+    let bg = Color(UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0))
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Registration")
+                .padding(50)
+            
+            TextField("Name", text: $name)
+                .padding(.horizontal)
+                .frame(height: 50)
+                .background(bg)
+                .cornerRadius(15)
+            
+            TextField("Surname", text: $surname)
+                .padding(.horizontal)
+                .frame(height: 50)
+                .background(bg)
+                .cornerRadius(15)
+            
+            TextField("Date of birth", text: $dateOfBirth)
+                .padding(.horizontal)
+                .frame(height: 50)
+                .background(bg)
+                .cornerRadius(15)
+            
+            TextField("Password", text: $password)
+                .padding(.horizontal)
+                .frame(height: 50)
+                .background(bg)
+                .cornerRadius(15)
+            
+            TextField("Repeat password", text: $repeatPassword)
+                .padding(.horizontal)
+                .frame(height: 50)
+                .background(bg)
+                .cornerRadius(15)
         }
         .padding()
     }
