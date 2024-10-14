2d9a5fe9a2e87d445ca3824a61250bc3a0553f54
bdvzt <budaevazay@mail.ru>
2024-10-14 17:15:10 +0700

    first commit
    created project

---
+++ SHIFT LAB/SHIFT LAB/SHIFT_LABApp.swift
@@ -0,0 +1,17 @@
+//
+//  SHIFT_LABApp.swift
+//  SHIFT LAB
+//
+//  Created by Zayata Budaeva on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SHIFT_LABApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
